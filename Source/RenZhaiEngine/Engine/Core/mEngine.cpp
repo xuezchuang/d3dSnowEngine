8087b6507225ccd494b7503a379e0eb69af7112c
xuezc <xuezc@midasit.com>
2025-01-02 09:55:05 +0800

    测试 Atmospheric 渲染.同时note:使用计算着色器和图形着色器分开计算时,资源要等待完成才可以使用

--- Source/RenZhaiEngine/Engine/Core/mEngine.cpp
+++ Source/RenZhaiEngine/Engine/Core/mEngine.cpp
@@ -8,8 +8,11 @@
 #include "../Rendering/Enigne/DirectX/Core/DirectXRenderingEngine.h"
 #include <CodeReflection/ClassManage.h>
 #include <CoreObject/ClassObject.h>
+#include "../Common.h"
+#include "../SamplerManager.h"
+#include "../CommandListManager.h"
 
-
+extern FCommandListManager g_CommandListManager;
 CEngine* Engine = NULL;
 
 void mEngine::OnStartup()
@@ -34,6 +37,24 @@ void mEngine::OnStartup()
 #if EDITOR_ENGINE
 	GetEditorEngine()->BuildEditor();
 #endif
+	const static bool bViewFromGround = true;
+	if(bViewFromGround)
+	{
+		float CameraHeight = 1000.f;
+		m_Camera = FCamera(Vector3f(0.f, CameraHeight, 0.f), Vector3f(0.f, CameraHeight, 1.f), Vector3f(0.f, 1.f, 0.f));
+
+		const float FovVertical = 65.f * MATH_PI / 180;
+		m_Camera.SetPerspectiveParams(FovVertical, (float)GetDesc().Width / GetDesc().Height, 0.1f, 100.f);
+	}
+	else
+	{
+		const static float AtmospherRadius = 6460e3;
+		m_Camera = FCamera(Vector3f(0.f, 0.f, -AtmospherRadius * 3), Vector3f(0.f, 0, 0.f), Vector3f(0.f, 1.f, 0.f));
+
+		const float FovVertical = MATH_PI / 4.f;
+		m_Camera.SetPerspectiveParams(FovVertical, (float)GetDesc().Width / GetDesc().Height, 0.1f, 100.f);
+	}
+	m_Atmospheric.OnStartup();
 }
 
 void mEngine::OnUpdate()
@@ -47,6 +68,7 @@ void mEngine::OnUpdate()
 	//}
 	//m_SizeX = curSizeX;
 	//m_SizeY = curSizeY;
+	m_Atmospheric.OnUpdate();
 }
 
 void mEngine::OnRender()
@@ -57,12 +79,13 @@ void mEngine::OnRender()
 	FColorBuffer& BackBuffer = renderWindow.GetBackBuffer();
 	
 	CommandContext.TransitionResource(BackBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET, true);
-	BackBuffer.SetClearColor(Vector3f(0.5f, 0.58f, 0.8f));
-	CommandContext.ClearColor(BackBuffer);
-
 	CommandContext.SetRenderTargets(1, &BackBuffer.GetRTV());
+	CommandContext.ClearColor(BackBuffer);
+	//CommandContext.FlushResourceBarriers();
 
-	OnTest(CommandContext);
+	//OnTest(CommandContext);
+
+	m_Atmospheric.OnRender(CommandContext);
 
 	OnGUI(CommandContext);
 	CommandContext.TransitionResource(BackBuffer, D3D12_RESOURCE_STATE_PRESENT);
@@ -109,3 +132,189 @@ CEditorEngine* mEngine::GetEditorEngine() const
 }
 #endif
 
+void Atmospheric::OnStartup()
+{
+	SetupCameraLight();
+	SetupMesh();
+	SetupShaders();
+	SetupPipelineState();
+}
+
+void Atmospheric::OnUpdate()
+{
+	m_Constants.ScreenParams = Vector4f(1.f * m_GameDesc->Width, 1.f * m_GameDesc->Height, 1.f / m_GameDesc->Width, 1.f / m_GameDesc->Height);
+	m_Constants.LightDirAndIntensity = Vector4f(m_DirectionLight.GetDirection(), m_DirectionLight.GetIntensity());
+	m_Constants.InvProjectionMatrix = m_Camera->GetProjectionMatrix().Inverse();
+	m_Constants.InvViewMatrix = m_Camera->GetViewMatrix().Inverse();
+}
+
+void Atmospheric::OnRender(FCommandContext& CommandContext)
+{
+	if(m_bUseComputeShader)
+		ScatteringPassCS(CommandContext);
+	else
+		ScatteringPassPS(CommandContext);
+	PostProcess(CommandContext);
+}
+
+void Atmospheric::SetupCameraLight()
+{
+	// parameters are from "A Scalable and Production Ready Sky and Atmosphere Rendering Technique"
+	const static float EarthRadius = 6360e3;
+	const static float AtmospherRadius = 6460e3;
+
+	m_Constants.DensityScaleHeight = Vector2f(8000.0f, 1200.f);
+	m_Constants.AtmosphereRadius = AtmospherRadius;
+	m_Constants.RayleiCoef = Vector3f(5.802f, 13.558f, 33.1f) * 1e-6f;
+	m_Constants.MieG = 0.8f; //[-1, 1], from backward to forward
+	m_Constants.MieCoef = 21e-6f;
+
+	const static bool bViewFromGround = true;
+	if(bViewFromGround)
+	{
+		float theta = 30 * MATH_PI / 180.f;
+		float z = cos(theta);
+		float y = sin(theta);
+		m_DirectionLight.SetDirection(Vector3f(0.f, -y, -z));
+		m_DirectionLight.SetIntensity(10.f);
+
+		m_Constants.EarthCenterAndRadius = Vector4f(0.f, -EarthRadius, 0.f, EarthRadius);
+	}
+	else
+	{
+		float theta = 90 * MATH_PI / 180.f;
+		float z = cos(theta);
+		float y = sin(theta);
+		//m_DirectionLight.SetDirection(Vector3f(-1.f, 0.f, -0.3f));
+		m_DirectionLight.SetDirection(Vector3f(0.f, 0.f, 1.0f));
+		m_DirectionLight.SetIntensity(20.f);
+
+		m_Constants.EarthCenterAndRadius = Vector4f(0.f, 0.f, 0.f, EarthRadius);
+	}
+}
+
+void Atmospheric::SetupMesh()
+{
+
+}
+
+void Atmospheric::SetupShaders()
+{
+	m_ScatteringCS = D3D12RHI::Get().CreateShader(getpath(L"Resources/Shaders/AtmosphericScatteringCS.hlsl"), "cs_main", "cs_5_1");
+	m_ScatteringPS = D3D12RHI::Get().CreateShader(getpath(L"Resources/Shaders/AtmosphericScatteringPS.hlsl"), "ps_main", "ps_5_1");
+	m_ScreenQuadVS = D3D12RHI::Get().CreateShader(getpath(L"Resources/Shaders/PostProcess.hlsl"), "VS_ScreenQuad", "vs_5_1");
+	m_PostPS = D3D12RHI::Get().CreateShader(getpath(L"Resources/Shaders/PostProcess.hlsl"), "PS_Main", "ps_5_1");
+
+	
+}
+
+void Atmospheric::SetupPipelineState()
+{
+	m_ScatteringSignature.Reset(3, 0);
+	m_ScatteringSignature[0].InitAsBufferCBV(0, D3D12_SHADER_VISIBILITY_ALL); // compute shader need 'all'
+	m_ScatteringSignature[1].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 0, 1);
+	m_ScatteringSignature[2].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0, 1);
+	m_ScatteringSignature.Finalize(L"Atmospheric Scattering RootSignature");
+
+	m_ScatteringCSPSO.SetRootSignature(m_ScatteringSignature);
+	m_ScatteringCSPSO.SetComputeShader(CD3DX12_SHADER_BYTECODE(m_ScatteringCS.Get()));
+	m_ScatteringCSPSO.Finalize();
+	m_ScatteringBuffer.Create(L"Scattering Buffer", m_GameDesc->Width, m_GameDesc->Height, 1, DXGI_FORMAT_R8G8B8A8_UNORM);
+
+	FSamplerDesc DefaultSamplerDesc;
+	m_PostSignature.Reset(2, 1);
+	m_PostSignature[0].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 0, 1);
+	m_PostSignature[1].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0, 1);
+	m_PostSignature.InitStaticSampler(0, DefaultSamplerDesc, D3D12_SHADER_VISIBILITY_PIXEL);
+	m_PostSignature.Finalize(L"Post Process RootSignature");
+
+	m_PostPSO.SetRootSignature(m_PostSignature);
+	m_PostPSO.SetRasterizerState(FPipelineState::RasterizerTwoSided);
+	m_PostPSO.SetBlendState(FPipelineState::BlendDisable);
+	m_PostPSO.SetDepthStencilState(FPipelineState::DepthStateDisabled);
+	// no need to set input layout
+	m_PostPSO.SetPrimitiveTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
+	m_PostPSO.SetRenderTargetFormats(1, &RenderWindow::Get().GetColorFormat(), DXGI_FORMAT_UNKNOWN);
+	m_PostPSO.SetVertexShader(CD3DX12_SHADER_BYTECODE(m_ScreenQuadVS.Get()));
+	m_PostPSO.SetPixelShader(CD3DX12_SHADER_BYTECODE(m_PostPS.Get()));
+	m_PostPSO.Finalize();
+
+	m_ScatteringPSPSO.SetRootSignature(m_ScatteringSignature);
+	m_ScatteringPSPSO.SetRasterizerState(FPipelineState::RasterizerTwoSided);
+	m_ScatteringPSPSO.SetBlendState(FPipelineState::BlendDisable);
+	m_ScatteringPSPSO.SetDepthStencilState(FPipelineState::DepthStateDisabled);
+	// no need to set input layout
+	m_ScatteringPSPSO.SetPrimitiveTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
+	m_ScatteringPSPSO.SetRenderTargetFormats(1, &RenderWindow::Get().GetColorFormat(), DXGI_FORMAT_UNKNOWN);
+	m_ScatteringPSPSO.SetVertexShader(CD3DX12_SHADER_BYTECODE(m_ScreenQuadVS.Get()));
+	m_ScatteringPSPSO.SetPixelShader(CD3DX12_SHADER_BYTECODE(m_ScatteringPS.Get()));
+	m_ScatteringPSPSO.Finalize();
+}
+
+void Atmospheric::ScatteringPassCS(FCommandContext& GfxContext)
+{
+	GfxContext.TransitionResource(m_ScatteringBuffer, D3D12_RESOURCE_STATE_UNORDERED_ACCESS, true);
+	g_CommandListManager.GetComputeQueue().WaitForFenceValue(GfxContext.Flush(true));
+
+	FComputeContext& CommandContext = FComputeContext::Begin(L"Compute Queue");
+
+	CommandContext.SetRootSignature(m_ScatteringSignature);
+	CommandContext.SetPipelineState(m_ScatteringCSPSO);
+
+
+	CommandContext.SetDynamicConstantBufferView(0, sizeof(m_Constants), &m_Constants);
+	CommandContext.SetDynamicDescriptor(2, 0, m_ScatteringBuffer.GetUAV());
+
+	uint32_t GroupCountX = (m_GameDesc->Width + 7) / 8;
+	uint32_t GroupCountY = (m_GameDesc->Height + 7) / 8;
+
+	CommandContext.Dispatch(GroupCountX, GroupCountY, 1);
+
+	CommandContext.Finish(false);
+}
+
+void Atmospheric::ScatteringPassPS(FCommandContext& GfxContext)
+{
+	GfxContext.SetRootSignature(m_ScatteringSignature);
+	GfxContext.SetPipelineState(m_ScatteringPSPSO);
+	GfxContext.SetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	GfxContext.SetViewportAndScissor(0, 0, m_GameDesc->Width, m_GameDesc->Height);
+
+	GfxContext.TransitionResource(m_ScatteringBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET, true);
+
+	GfxContext.SetRenderTargets(1, &m_ScatteringBuffer.GetRTV());
+	GfxContext.ClearColor(m_ScatteringBuffer);
+
+	GfxContext.SetDynamicConstantBufferView(0, sizeof(m_Constants), &m_Constants);
+
+	GfxContext.Draw(3);
+}
+
+void Atmospheric::PostProcess(FCommandContext& GfxContext)
+{
+	if(m_bUseComputeShader)
+	{
+		g_CommandListManager.GetGraphicsQueue().StallForProducer(g_CommandListManager.GetComputeQueue());
+	}
+	// Set necessary state.
+	GfxContext.SetRootSignature(m_PostSignature);
+	GfxContext.SetPipelineState(m_PostPSO);
+	GfxContext.SetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	GfxContext.SetViewportAndScissor(0, 0, m_GameDesc->Width, m_GameDesc->Height);
+
+	RenderWindow& renderWindow = RenderWindow::Get();
+	FColorBuffer& BackBuffer = renderWindow.GetBackBuffer();
+
+	// Indicate that the back buffer will be used as a render target.
+	GfxContext.TransitionResource(m_ScatteringBuffer, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+	GfxContext.TransitionResource(BackBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET, true);
+
+	GfxContext.SetRenderTargets(1, &BackBuffer.GetRTV());
+
+	GfxContext.ClearColor(BackBuffer);
+
+	GfxContext.SetDynamicDescriptor(0, 0, m_ScatteringBuffer.GetSRV());
+
+	// no need to set vertex buffer and index buffer
+	GfxContext.Draw(3);
+}
--- Source/RenZhaiEngine/Engine/Core/mEngine.h
+++ Source/RenZhaiEngine/Engine/Core/mEngine.h
@@ -1,15 +1,76 @@
 #pragma once
 #include "include/Game.h"
 #include "include/CommandContext.h"
+#include "include/D3D12RHI.h"
+#include "include/d3dx12.h"
+#include "include/RootSignature.h"
+#include "include/PipelineState.h"
+#include "include/ColorBuffer.h"
+#include "include/Camera.h"
+#include "include/Light.h"
 
 class CEditorEngine;
 class CDirectXRenderingEngine;
 
+class Atmospheric
+{
+public:
+
+	void OnStartup();
+	void OnUpdate();
+	void OnRender(FCommandContext& CommandContext);
+
+	FCamera* m_Camera;
+	GameDesc* m_GameDesc;
+private:
+	void SetupCameraLight();
+	void SetupMesh();
+	void SetupShaders();
+	void SetupPipelineState();
+	void ScatteringPassCS(FCommandContext& GfxContext);
+	void ScatteringPassPS(FCommandContext& GfxContext);
+	void PostProcess(FCommandContext& GfxContext);
+
+private:
+	__declspec(align(16)) struct
+	{
+		FMatrix		InvProjectionMatrix;
+		FMatrix		InvViewMatrix;
+		Vector4f	ScreenParams;
+		Vector4f	LightDirAndIntensity;
+		Vector4f	EarthCenterAndRadius;
+		Vector2f	DensityScaleHeight;
+		float		AtmosphereRadius;
+		float		MieG;
+		float		MieCoef;
+		Vector3f	RayleiCoef;
+	} m_Constants;
+
+	FRootSignature m_ScatteringSignature;
+	FRootSignature m_PostSignature;
+
+	FComputePipelineState m_ScatteringCSPSO;
+	FGraphicsPipelineState m_ScatteringPSPSO;
+	FGraphicsPipelineState m_PostPSO;
+
+	FColorBuffer m_ScatteringBuffer;
+
+	ComPtr<ID3DBlob> m_ScatteringCS;
+	ComPtr<ID3DBlob> m_ScreenQuadVS;
+	ComPtr<ID3DBlob> m_PostPS;
+	ComPtr<ID3DBlob> m_ScatteringPS;
+
+	bool m_bUseComputeShader = true;
+
+	FDirectionalLight m_DirectionLight;
+};
 class mEngine : public FGame
 {
 public:
 	mEngine(const GameDesc& Desc) : FGame(Desc)
 	{
+		m_Atmospheric.m_Camera = &m_Camera;
+		m_Atmospheric.m_GameDesc = &m_GameDesc;
 	}
 
 	virtual void OnStartup() override;
@@ -28,4 +89,6 @@ private:
 	CEditorEngine* GetEditorEngine()const;
 	CDirectXRenderingEngine* GetDirectRenderingEngine()const;
 
+	Atmospheric m_Atmospheric;
+	FCamera m_Camera;
 };
